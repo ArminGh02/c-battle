f647ae7f860ebef6c16f4bf696ccfb3ff69f6a01
armingh02 <arminghorbanian02@gmail.com>
2021-01-25 19:54:18 +0330

    init commit

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void main_menu();
+
+void pause_menu();
+
+void help();
+
+void play_vs_bot();
+
+void display_players_board();
+
+void display_opponents_board();
+
+void print_header();
+void print_date();
+void print_footer();
+
+void play_vs_a_friend();
+
+void classic_mode();
+
+void advanced_mode();
+void equip_army();
+void use_radar();
+void launch_rocket();
+
+void auto_place_ships();
+
+void manually_place_ships();
+
+void save_ships_arrangement();
+
+void load_ships_arrangement();
+
+bool is_valid_placement();
+
+void apply_changes();
+
+bool is_valid_shoot();
+
+void save_shoots();
+
+void save_game();
+
+void load_game();
+void display_saved_games_list();
+void is_game_finished();
+void replay();
+void resume_game();
+
+void display_scoreboard();
+
+int main() {
+
+    return 0;
+}
